9f3105a1de4bf37a5d8e686066de4c0b71051c09
dfreire <dario.freire@gmail.com>
2015-06-10 22:04:15 +0100

    update

--- client/js/home.jsx
+++ client/js/home.jsx
@@ -4,7 +4,32 @@ import Router from "react-router";
 export default React.createClass({
     mixins: [Router.State],
 
+    componentDidMount: function() {
+        var i = 0;
+        var imgs = ["/img/LP-OCCUPATION.jpg", "/img/LP-OCUPACAO.jpg"];
+        setInterval(function(){
+            var img = imgs[i++];
+            if (i === 2) {
+                i = 0;
+            }
+
+            $('#homeImg').fadeOut(500, function() {
+                console.log("go go go", img);
+                $('#homeImg').attr("src", img);
+                $('#homeImg').fadeIn(500);
+            });
+        }, 2000);
+    },
+
     render: function() {
-        return (<h1>Home</h1>);
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <div>
+                        <img id="homeImg1" style={{width: "100%"}} src="/img/LP-OCUPACAO.jpg" />
+                    </div>
+                </div>
+            </div>
+        );
     }
 });
