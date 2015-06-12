7755d3de52c1bc93cf7bd1bb9985a9d1cf0f14ce
dfreire <dario.freire@gmail.com>
2015-06-12 22:37:55 +0100

    update

--- client/js/texts.jsx
+++ client/js/texts.jsx
@@ -5,6 +5,18 @@ export default React.createClass({
     mixins: [Router.State],
 
     render: function() {
-        return (<h1>Texts</h1>);
+        return (
+            <ul style={{listStyleType: "none", fontSize: "11px", paddingLeft: 0}}>
+                <li>Paisagens Periféricas, Memória e Recordação</li>
+                <li>Bascos</li>
+                <li>Paisagem, Indústria, Memória</li>
+                <li>Silentes</li>
+                <li>Territorialidade</li>
+                <li>Factos e Ficções, a propósito de Territorialidade.</li>
+                <li>Um diálogo com António Soares, a propósito de ‘Ocupação’.</li>
+                <li>Sobre a fotografia da Estação Internacional de Canfranc.</li>
+                <li>Dar a ver as cicatrizes da História</li>
+            </ul>
+        );
     }
 });
