c802c79ffec1a86f4bd1a5b581e449d9e7d6a09d
dfreire <dario.freire@gmail.com>
2015-07-12 15:09:26 +0100

    update

--- client/js/projects.jsx
+++ client/js/projects.jsx
@@ -8,29 +8,29 @@ export default React.createClass({
         return (
             <div className="row">
                 <div className="col-md-4">
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/tm-c05dd.jpg" />
                     </div>
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/tm-d452d.jpg" />
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/6.jpg" />
                     </div>
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/L1040611.jpg" />
                     </div>
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/tm-3b09a.jpg" />
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/tm-dfd2b.jpg" />
                     </div>
-                    <div style={{marginBottom: "10px"}}>
+                    <div style={{marginBottom: "30px"}}>
                         <img style={{width: "100%"}} src="/img/tm-c70b2.jpg" />
                     </div>
                 </div>
