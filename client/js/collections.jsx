869d70d3bc73519122354b30bfc997070b565ee3
dfreire <dario.freire@gmail.com>
2015-07-14 22:06:43 +0100

    update

--- client/js/collections.jsx
+++ client/js/collections.jsx
@@ -5,6 +5,42 @@ export default React.createClass({
     mixins: [Router.State],
 
     render: function() {
-        return (<h1>Collections</h1>);
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/tm-126d0.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/tm-374e0.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/tm-99435.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/tm-bff08.jpg" />
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/topografias.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/tm-ed8a6.jpg" />
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/EDP.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/BESarte.jpg" />
+                    </div>
+                    <div style={{marginBottom: "30px"}}>
+                        <img style={{width: "100%"}} src="/img/collections/rekalde.jpg" />
+                    </div>
+                </div>
+            </div>
+        );
     }
 });
