64ec5afa088ceacf20d5b8e94a0f428dad7f5134
dfreire <dario.freire@gmail.com>
2015-08-15 16:02:41 +0100

    update

--- client/js/video.jsx
+++ client/js/video.jsx
@@ -1,4 +1,8 @@
 // https://www.youtube.com/watch?v=_oeOwE-xlXQ
+// width="790" height="444"
+// width="560" height="349"
+// https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+
 
 import React from "react/addons";
 import Router from "react-router";
@@ -8,12 +12,13 @@ export default React.createClass({
 
     render: function() {
         return (
-            <iframe style={{border: 0}}
-                className="youtube-player" type="text/html"
-                width="790" height="444"
-                src="http://www.youtube.com/embed/_oeOwE-xlXQ"
-                frameborder="0"
-                allowFullScreen></iframe>
+            <div className="videoWrapper">
+                <iframe style={{border: 0}}
+                    className="youtube-player" type="text/html"
+                    src="http://www.youtube.com/embed/_oeOwE-xlXQ"
+                    allowFullScreen></iframe>
+            </div>
+
         );
     }
 });
