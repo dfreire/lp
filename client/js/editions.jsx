20d9f6013076310d7b1653d7bfe966f30da71fc7
dfreire <dario.freire@gmail.com>
2015-08-27 22:54:42 +0100

    update

--- client/js/editions.jsx
+++ client/js/editions.jsx
@@ -1,20 +1,21 @@
 import React from "react/addons";
 import Router from "react-router";
 
+import LinksPage from "./components/links-page.jsx";
+
+const links = [
+    { href:"#", caption: "Ocupação" },
+    { href:"#", caption: "Anatomia de um Sonho" },
+    { href:"#", caption: "Mapeamento, Memória, Política" },
+    { href:"#", caption: "From East to West" }
+];
+
 export default React.createClass({
     mixins: [Router.State],
 
     render: function() {
-        var liStyle = {
-            lineHeight: "30px"
-        };
         return (
-            <ul style={{listStyleType: "none", fontSize: "11px", paddingLeft: 0}}>
-                <li style={liStyle}>Ocupação</li>
-                <li style={liStyle}>Anatomia de um Sonho</li>
-                <li style={liStyle}>Mapeamento, Memória, Política</li>
-                <li style={liStyle}>From East to West</li>
-            </ul>
+            <LinksPage links={links} />
         );
     }
 });
