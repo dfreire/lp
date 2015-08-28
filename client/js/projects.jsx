ac79f161f4aa58d246a9ff8141fad2a24256a761
dfreire <dario.freire@gmail.com>
2015-08-28 22:59:39 +0100

    update

--- client/js/projects.jsx
+++ client/js/projects.jsx
@@ -1,64 +1,38 @@
 import React from "react/addons";
 import Router from "react-router";
 
+import PhotosPage from "./components/photos-page.jsx";
+
+let col1 = [
+    { src: "/img/projects/tm-c05dd.jpg" },
+    { src: "/img/projects/tm-d452d.jpg" },
+    { src: "/img/projects/tm-dfd2b.jpg" },
+    { src: "/img/projects/tm-c70b2.jpg" },
+    { src: "/img/projects/6.jpg" }
+];
+
+let col2 = [
+    { src: "/img/projects/6.jpg" },
+    { src: "/img/projects/L1040611.jpg" },
+    { src: "/img/projects/tm-3b09a.jpg" },
+    { src: "/img/projects/tm-c05dd.jpg" },
+    { src: "/img/projects/tm-d452d.jpg" }
+];
+
+let col3 = [
+    { src: "/img/projects/tm-dfd2b.jpg" },
+    { src: "/img/projects/tm-c70b2.jpg" },
+    { src: "/img/projects/6.jpg" },
+    { src: "/img/projects/L1040611.jpg" },
+    { src: "/img/projects/tm-3b09a.jpg" }
+];
+
 export default React.createClass({
     mixins: [Router.State],
 
     render: function() {
         return (
-            <div className="row">
-                <div className="col-md-4">
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-c05dd.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-d452d.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-dfd2b.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-c70b2.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/6.jpg" />
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/6.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/L1040611.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-3b09a.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-c05dd.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-d452d.jpg" />
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-dfd2b.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-c70b2.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/6.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/L1040611.jpg" />
-                    </div>
-                    <div style={{marginBottom: "30px"}}>
-                        <img style={{width: "100%"}} src="/img/projects/tm-3b09a.jpg" />
-                    </div>
-                </div>
-            </div>
+            <PhotosPage col1={col1} col2={col2} col3={col3} />
         );
     }
 });
