cbfdd5cd4f86f35eabf878fb4a1e05b428b681a2
dfreire <dario.freire@gmail.com>
2015-08-28 22:43:20 +0100

    update

--- client/js/texts.jsx
+++ client/js/texts.jsx
@@ -1,25 +1,26 @@
 import React from "react/addons";
 import Router from "react-router";
 
+import LinksPage from "./components/links-page.jsx";
+
+const links = [
+    { href:"#", caption: "Paisagens Periféricas, Memória e Recordação" },
+    { href:"#", caption: "Bascos" },
+    { href:"#", caption: "Paisagem, Indústria, Memória" },
+    { href:"#", caption: "Silentes" },
+    { href:"#", caption: "Territorialidade" },
+    { href:"#", caption: "Factos e Ficções, a propósito de Territorialidade." },
+    { href:"#", caption: "Um diálogo com António Soares, a propósito de ‘Ocupação’." },
+    { href:"#", caption: "Sobre a fotografia da Estação Internacional de Canfranc." },
+    { href:"#", caption: "Dar a ver as cicatrizes da História" }
+];
+
 export default React.createClass({
     mixins: [Router.State],
 
     render: function() {
-        var liStyle = {
-            lineHeight: "30px"
-        };
         return (
-            <ul style={{listStyleType: "none", fontSize: "11px", paddingLeft: 0}}>
-                <li style={liStyle}>Paisagens Periféricas, Memória e Recordação</li>
-                <li style={liStyle}>Bascos</li>
-                <li style={liStyle}>Paisagem, Indústria, Memória</li>
-                <li style={liStyle}>Silentes</li>
-                <li style={liStyle}>Territorialidade</li>
-                <li style={liStyle}>Factos e Ficções, a propósito de Territorialidade.</li>
-                <li style={liStyle}>Um diálogo com António Soares, a propósito de ‘Ocupação’.</li>
-                <li style={liStyle}>Sobre a fotografia da Estação Internacional de Canfranc.</li>
-                <li style={liStyle}>Dar a ver as cicatrizes da História</li>
-            </ul>
+            <LinksPage links={links} />
         );
     }
 });
